641d124fad9a6390c4e323efd4c77bcb6b0cf781
Brightcells <brightcells@gmail.com>
2018-03-29 14:35:21 +0800

    :art: Add param isdigit for dict_to_xml

--- pywe_xml/pywexml.py
+++ pywe_xml/pywexml.py
@@ -12,11 +12,11 @@ from pywe_utils import to_text
 __all__ = ['dict_to_xml', 'xml_to_dict']
 
 
-def dict_to_xml(d, ignore=True):
+def dict_to_xml(d, ignore=True, isdigit=True):
     xml = ['<xml>\n']
     for k, v in d.items():
         if (ignore and v) or (not ignore):
-            if isinstance(v, six.integer_types) or v.isdigit():
+            if isinstance(v, six.integer_types) or (isdigit and v.isdigit()):
                 xml.append('<{0}>{1}</{0}>\n'.format(to_text(k), to_text(v)))
             else:
                 xml.append('<{0}><![CDATA[{1}]]></{0}>\n'.format(to_text(k), to_text(v)))
