68b96e30fb044861f2f8ca95b31e1771d09b9f10
Brightcells <brightcells@gmail.com>
2018-04-23 17:49:28 +0800

    :art: Support [('a', 1), ('b', 2), ('c', [[('d', 4)], [('e', 5)]])...]

--- pywe_xml/pywexml.py
+++ pywe_xml/pywexml.py
@@ -40,20 +40,27 @@ def xml_to_dict(x):
 
 
 def pair_to_xml(listuple, ignore=True, isdigit=True, subxml=False):
-    """ [('a', 1), ('b', 2), ('c', [('d', 4)])...] """
+    """
+    [('a', 1), ('b', 2), ('c', [('d', 4)])...]
+    [('a', 1), ('b', 2), ('c', [[('d', 4)], [('e', 5)]])...]
+    """
     # If subxml, add '<xml>\n'
     xml = [] if subxml else ['<xml>\n']
     # Foreach joint kv
-    for k, v in listuple:
-        if (ignore and v) or (not ignore):
-            # If ``v`` is dict.
-            # Loop call dict_to_xml
-            if isinstance(v, list):
-                xml.append('<{0}>\n{1}</{0}>\n'.format(to_text(k), pair_to_xml(v, subxml=True)))
-            elif isinstance(v, six.integer_types) or (isdigit and v.isdigit()):
-                xml.append('<{0}>{1}</{0}>\n'.format(to_text(k), to_text(v)))
-            else:
-                xml.append('<{0}><![CDATA[{1}]]></{0}>\n'.format(to_text(k), to_text(v)))
+    try:
+        for k, v in listuple:
+            if (ignore and v) or (not ignore):
+                # If ``v`` is dict.
+                # Loop call dict_to_xml
+                if isinstance(v, list):
+                    xml.append('<{0}>\n{1}</{0}>\n'.format(to_text(k), pair_to_xml(v, subxml=True)))
+                elif isinstance(v, six.integer_types) or (isdigit and v.isdigit()):
+                    xml.append('<{0}>{1}</{0}>\n'.format(to_text(k), to_text(v)))
+                else:
+                    xml.append('<{0}><![CDATA[{1}]]></{0}>\n'.format(to_text(k), to_text(v)))
+    except ValueError:
+        for ltup in listuple:
+            xml.append('<item>\n{0}</item>\n'.format(pair_to_xml(ltup, subxml=True)))
     # If subxml, add '</xml>'
     if not subxml:
         xml.append('</xml>')
